70e40e84665acad571aeb2a63cd4aa7eb6ec0d9c
Mustafakonya <av.mustafakonya@gmail.com>
2020-09-10 22:24:00 +0200

    day17 ders sonu

---
+++ src/test/java/com/techproed/pages/GooglePage.java
@@ -0,0 +1,26 @@
+package com.techproed.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class GooglePage {
+
+    WebDriver driver;
+
+    public GooglePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(name = "q")
+    public WebElement aramaKtusu;     //findElement
+
+    @FindBy(xpath = "//tbody/tr")
+    public List<WebElement> liste;   //findElements (s harfine dikkat)
+
+
+}
